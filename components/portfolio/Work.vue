36e80d1960fe0711be47c83523516d990c756c61
romain sanson <romainsanson.gm@gmail.com>
2021-06-09 22:38:20 +0200

    cleanup

--- components/portfolio/Work.vue
+++ components/portfolio/Work.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="`work-card-${work.id} bg-secondary-dark flex items-center justify-around p-4 cursor-pointer`">
+  <div :class="`work-card-${work.id} work-card`">
     <img src="https://images.unsplash.com/photo-1519389950473-47ba0277781c?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80" alt="">
     <div class="w-2/3 p-4">
       <p class="text-secondary-grey font-semibold opacity-80">Nov 21</p>
