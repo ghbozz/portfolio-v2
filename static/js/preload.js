4e834e75821fda1984fb5b6e48fe83270d78b279
romain sanson <romainsanson.gm@gmail.com>
2021-06-12 00:38:31 +0200

    cleanup

--- static/js/preload.js
+++
@@ -1,9 +0,0 @@
-// const clear_preload = () => {
-//   document.querySelectorAll(".preload").forEach(item => {
-//     item.classList.remove("preload");
-//   });
-// };
-
-// window.addEventListener("DOMContentLoaded", () => {
-//   setTimeout(clear_preload, 1000);
-// });
