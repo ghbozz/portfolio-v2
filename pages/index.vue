a243c7c2de68241e6c50233f5881b4bd34b6cd4f
romain sanson <romainsanson.gm@gmail.com>
2021-06-12 01:23:24 +0200

    wip

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@
           <span class="text-gradient-light pl-1">romain</span><span class="font-extralight opacity-70 text-primary-light">sanson</span><span class="text-gradient-green font-semibold pr-1 glow">.dev</span>
         </h1>
       </transition>
-      <button id="contact-button" class="main-btn relative mb-4 vim-mode">
+      <button @click="toggle_contact" id="contact-button" class="main-btn relative mb-4 vim-mode">
         contact
       </button>
       <div class="preload" ref="preload">
@@ -24,33 +24,27 @@ export default {
     }
   },
   methods: {
-  //   toggle_contact() {
-  //     this.opened = !this.opened;
+    toggle_contact() {
+      this.opened = !this.opened;
 
-  //     if (this.opened) {
-  //       this.contact_button.classList.add('active');
-  //       this.form.classList.add('opacity-100');
-  //     } else {
-  //       this.contact_button.classList.remove('active');
-  //       this.form.classList.remove('opacity-100');
+      if (this.opened) {
+        document.querySelector('#contact-button').classList.add('active');
+        document.querySelector('form').classList.add('opacity-100');
+      } else {
+        document.querySelector('#contact-button').classList.remove('active');
+        document.querySelector('form').classList.remove('opacity-100');
 
-  //       this.labels.forEach((label) => {
-  //         label.classList.remove('active');
-  //       })
-  //     }
-  //   },
-  //   setup() {
-  //     this.form = document.querySelector('form');
-  //     this.labels = document.querySelectorAll('label');
-  //     this.contact_button = document.querySelector('#contact-button')
-  //   },
-  //   clear_preload() {
-  //     this.$refs.preload.classList.remove('preload')
-  //   }
-  // },
-  // beforeMount() {
-  //   this.setup();
-  //   setTimeout(this.clear_preload, 800)
+        document.querySelectorAll('label').forEach((label) => {
+          label.classList.remove('active');
+        })
+      }
+    },
+    clear_preload() {
+      this.$refs.preload.classList.remove('preload')
+    }
+  },
+  mounted() {
+    setTimeout(this.clear_preload, 800)
   }
 }
 </script>
