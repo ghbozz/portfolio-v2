2f72a5fd34d5bf37bc2dcafb99f5f17c4c43509f
romain sanson <romainsanson.gm@gmail.com>
2021-06-13 01:44:52 +0200

    about wip

--- pages/about.vue
+++ pages/about.vue
@@ -1,21 +1,12 @@
 <template>
-  <div id="about" class="w-full h-full bg-primary-dark"
-       :style="{ backgroundImage: 'url(' + background + ')' }">
-
+  <div id="about" class="w-full h-full bg-primary-dark">
     <div class="container mx-auto h-screen flex items-center justify-center flex-col">
-
     </div>
   </div>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      background : require('~/assets/images/about-bg.jpg'),
-    }
-  },
-}
+export default {}
 </script>
 
 <style lang="scss">
