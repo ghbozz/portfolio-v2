54c403f611ad1f9006fe7a3c480e0b7b003a2858
romain sanson <romainsanson.gm@gmail.com>
2021-06-13 10:50:17 +0200

    txeak

--- components/shared/Nav.vue
+++ components/shared/Nav.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-screen flex items-center justify-between fixed top-0 px-12 h-24"
+  <nav class="w-screen flex items-center justify-between fixed top-0 px-12 h-24"
        :class="$nuxt.$route.path == '/' ? 'flex-row-reverse' : 'flex-row'">
 
     <NuxtLink to="/" 
@@ -12,7 +12,7 @@
       <li class="nav-item"><NuxtLink to="/portfolio">portfolio</NuxtLink></li>
       <li class="nav-item"><NuxtLink to="/about">about</NuxtLink></li>
     </ul>
-  </div>
+  </nav>
 </template>
 
 <script>
