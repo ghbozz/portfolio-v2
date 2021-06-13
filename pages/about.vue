65c504f0ea0b2b70a36968ab1f96a8bd9b2de0ea
romain sanson <romainsanson.gm@gmail.com>
2021-06-13 23:08:53 +0200

    cleanup

--- pages/about.vue
+++ pages/about.vue
@@ -1,6 +1,7 @@
 <template>
-  <div id="about" class="w-full h-full bg-primary-dark">
+  <div id="about" class="w-full h-full dark-break-secondary">
     <div class="container mx-auto h-screen flex items-center justify-center flex-col">
+      <img src="~/assets/images/about-bg.jpg" alt="">
     </div>
   </div>
 </template>
