a6f04e88f0a33d60322893a2520f4244e272f444
romain sanson <romainsanson.gm@gmail.com>
2021-06-14 17:03:06 +0200

    cleanup

--- pages/about.vue
+++ pages/about.vue
@@ -1,13 +1,13 @@
 <template>
   <div id="about" class="w-full h-full dark-break-secondary relative">
     <div class="container mx-auto h-screen flex items-center justify-center flex-col">
-      <div id="vue" class="rounded-full w-40 h-40 absolute dark-linear dev-icon flex items-center justify-center">
+      <div id="vue" class="rounded-full w-40 h-40 absolute dark-linear dev-icon glass flex items-center justify-center shadow-lg ring-2 ring-secondary-light ring-opacity-30">
         <img src="~/assets/icons/dev/vue.png" alt="">
       </div>
-      <div id="ror" class="rounded-full w-40 h-40 absolute dark-linear dev-icon flex items-center justify-center">
+      <div id="ror" class="rounded-full w-40 h-40 absolute dark-linear dev-icon glass flex items-center justify-center shadow-lg ring-2 ring-secondary-light ring-opacity-30">
         <img class="dev-icon" src="~/assets/icons/dev/ruby-flat.png" alt="">
       </div>
-      <div id="js" class="rounded-full w-40 h-40 absolute dark-linear dev-icon flex items-center justify-center">
+      <div id="js" class="rounded-full w-40 h-40 absolute dark-linear dev-icon glass flex items-center justify-center shadow-lg ring-2 ring-secondary-light ring-opacity-30">
         <img class="dev-icon" src="~/assets/icons/dev/javascript.png" alt="">
       </div>
       <img id="hoodie" src="~/assets/images/about-bg.jpg" alt="">
@@ -33,6 +33,8 @@ export default {
     img {
       width: 60%;
       margin-top: 15px;
+      -webkit-filter: drop-shadow(3px 3px 3px #222);
+      filter: drop-shadow(3px 3px 3px #222);
     }
   }
 
@@ -43,6 +45,8 @@ export default {
     img {
       width: 70%;
       margin-top: 15px;
+      -webkit-filter: drop-shadow(3px 3px 3px #222);
+      filter: drop-shadow(3px 3px 3px #222);
     }
   }
 
@@ -53,6 +57,8 @@ export default {
     img {
       width: 60%;
       margin-top: 15px;
+      -webkit-filter: drop-shadow(3px 3px 3px #222);
+      filter: drop-shadow(3px 3px 3px #222);
     }
   }
 </style>
