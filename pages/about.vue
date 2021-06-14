4756841a7a82a3e8c1adf7da6554f6615c46ac80
romain sanson <romainsanson.gm@gmail.com>
2021-06-14 15:12:00 +0200

    cleanup

--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,16 @@
 <template>
   <div id="about" class="w-full h-full dark-break-secondary relative">
     <div class="container mx-auto h-screen flex items-center justify-center flex-col">
-      <img id="vue" class="dev-icon" src="~/assets/icons/dev/vue.png" alt="">
-      <img id="ror" class="dev-icon" src="~/assets/icons/dev/ruby-flat.png" alt="">
-      <img id="js" class="dev-icon" src="~/assets/icons/dev/javascript.png" alt="">
-      <img src="~/assets/images/about-bg.jpg" alt="">
+      <div id="vue" class="rounded-full w-40 h-40 absolute dark-linear dev-icon flex items-center justify-center">
+        <img src="~/assets/icons/dev/vue.png" alt="">
+      </div>
+      <div id="ror" class="rounded-full w-40 h-40 absolute dark-linear dev-icon flex items-center justify-center">
+        <img class="dev-icon" src="~/assets/icons/dev/ruby-flat.png" alt="">
+      </div>
+      <div id="js" class="rounded-full w-40 h-40 absolute dark-linear dev-icon flex items-center justify-center">
+        <img class="dev-icon" src="~/assets/icons/dev/javascript.png" alt="">
+      </div>
+      <img id="hoodie" src="~/assets/images/about-bg.jpg" alt="">
     </div>
   </div>
 </template>
@@ -18,24 +24,35 @@ export default {
 <style scoped lang="scss">
   .dev-icon {
     position: absolute;
-    filter: grayscale(50%);
   }
 
   #vue {
-    left: 5%;
-    bottom: 5%;
-    width: 100px;
+    left: 3%;
+    bottom: 7%;
+    
+    img {
+      width: 60%;
+      margin-top: 15px;
+    }
   }
 
   #ror {
     top: 30%;
     left: 10%;
-    width: 130px;
+
+    img {
+      width: 70%;
+      margin-top: 15px;
+    }
   }
 
   #js {
-    bottom: 20%;
+    bottom: 15%;
     left: 55%;
-    width: 100px;
+
+    img {
+      width: 60%;
+      margin-top: 15px;
+    }
   }
 </style>
