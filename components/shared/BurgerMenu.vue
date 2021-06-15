65b847e4cc181ab2dd6d531185db384cd10e89d6
romain sanson <romainsanson.gm@gmail.com>
2021-06-15 14:44:32 +0200

    cleanup

--- components/shared/BurgerMenu.vue
+++ components/shared/BurgerMenu.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="md:hidden">
+  <div class="md:hidden" :class="this.$store.state.mobile.menu ? 'green' : ''">
     <div class="grid cursor-pointer absolute px-8" @click="click">
       <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-square text-primary-light">
         <rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect>
@@ -37,5 +37,9 @@ export default {
     top: 25px;
     right: 0;
     z-index: 999;
+  }
+
+  .green {
+    fill: #05a672;
   }
 </style>
