e39124d3ecbb7df78696ee41865db403bc5f79d0
romain sanson <romainsanson.gm@gmail.com>
2021-06-17 22:57:38 +0200

    48de694

---
+++ _nuxt/static/1623963457/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b,c){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1623963457",layout:"default",error:a,state:{mobile:{menu:b},notice:{notice:b,status:a}},serverRendered:true,routePath:c,config:{_app:{basePath:c,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"d090e4e5"}}}}(null,false,"\u002F"));
