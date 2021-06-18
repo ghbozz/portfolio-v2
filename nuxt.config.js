41bd525d0b37713547dc16160920f2b518a56788
romain sanson <romainsanson.gm@gmail.com>
2021-06-18 11:06:19 +0200

    switch back to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
    ** Nuxt target
    ** See https://nuxtjs.org/api/configuration-target
    */
-  target: "server",
+  target: "static",
   // Router
   router: {
     base: process.env.NODE_ENV === "production" ? "/" : "/"
